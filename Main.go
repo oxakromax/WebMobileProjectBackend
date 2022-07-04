1b43b7359f2612f06be53f764a020ddc14d09bb2
Nicolás Hidalgo <nhidalgos@uft.edu>
2022-07-03 23:13:59 -0400

    make it better

--- Main.go
+++ Main.go
@@ -34,7 +34,7 @@ func main() {
 	app.Post("/upload", UploadRoutine())
 	app.Post("/delete", DeleteRoutine())
 	app.Get("/getimages", GetRoutine())
-	err := app.Listen(":6000")
+	err := app.Listen(":6321")
 	if err != nil {
 		return
 	}
